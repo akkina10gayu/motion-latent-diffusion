5ae1890df951fccd89b5779c4bcd16689fe09f66
Avinash Amballa <amballaavinash@gmail.com>
2024-05-09 02:50:40 -0400

    test

--- trainer_bash.sh
+++ trainer_bash.sh
@@ -4,7 +4,7 @@
 #SBATCH --mem=40G  # Requested Memory
 #SBATCH -p gpu-preempt  # Partition
 #SBATCH -G 1  # Number of GPUs
-#SBATCH -t 12:00:00  # Job time limit
+#SBATCH -t 25:00:00  # Job time limit
 #SBATCH -o slurm-%j.out  # %j = job ID
 #SBATCH --constraint=[a100]
 export PYTHONPATH="${PYTHONPATH}=$(pwd):$PYTHONPATH"
