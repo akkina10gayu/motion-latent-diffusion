f707000911b7b885b375d285d9d0c3750abea948
Avinash Amballa <amballaavinash@gmail.com>
2024-05-11 03:31:44 -0400

    test

--- mld/models/modeltype/mld.py
+++ mld/models/modeltype/mld.py
@@ -147,11 +147,8 @@ class MLD(BaseModel):
         load T2M text encoder and motion encoder for evaluating
         """
         # init module
-        print(cfg.model.t2m_textencoder.dim_word)
-        print(cfg.model.t2m_textencoder.dim_pos_ohot)
-        print(cfg.model.t2m_textencoder.dim_text_hidden)
-        print(cfg.model.t2m_textencoder.dim_coemb_hidden)
-        
+        print(cfg.model.t2m_motionencoder.dim_move_hidden)
+        print(cfg.model.t2m_motionencoder.dim_move_latent)
         
         self.t2m_textencoder = t2m_textenc.TextEncoderBiGRUCo(
             word_size=cfg.model.t2m_textencoder.dim_word,
